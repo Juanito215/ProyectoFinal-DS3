aaffef8e8dcdfca9c511853bd7aa58606cfe0d84
ImMavz <josephherrera1407@gmail.com>
2025-12-15 01:57:33 -0500

    Pequeños cambios :)

---
+++ Estructura/barber.service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO barberia (id, nombre, direccion)
+VALUES
+(1, 'Barbería Central', 'Calle 10'),
+(2, 'Golden Cut', 'Carrera 15');
+
+INSERT INTO barbero (id, nombre, barberia_id)
+VALUES
+(1, 'Juan', 1),
+(2, 'Carlos', 2);
---
+++ Estructura/db/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE auth_db;
+CREATE DATABASE barber_db;
+CREATE DATABASE appointment_db;

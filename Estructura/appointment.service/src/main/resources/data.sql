2672b98f6d64fb83c11771d7c719e00768512240
Juanito <juan.david.cuellar@correounivalle.edu.co>
2025-12-15 19:58:18 -0500

    Se sube los data sql para auth y appointment

---
+++ Estructura/appointment.service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO appointments (id, date, time, status, user_id, barber_id, service_id)
+VALUES
+(1, '2025-12-16', '10:00', 'PENDING', 2, 1, 1),
+(2, '2025-12-16', '11:30', 'CONFIRMED', 2, 1, 2),
+(3, '2025-12-17', '15:00', 'CANCELLED', 3, 2, 3);
